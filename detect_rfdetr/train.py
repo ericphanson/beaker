754830d4dcc9804dc70a02c131a6ff4af33b505b
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-08-12 23:59:28 +0200

    wip

--- detect_rfdetr/train.py
+++ detect_rfdetr/train.py
@@ -1,7 +1,21 @@
+import comet_ml
+import os
+
 from rfdetr import RFDETRNano
+
+api_key = os.getenv("COMET_API_KEY")
+
+project_name = os.getenv("COMET_PROJECT_NAME", "bird-head-detector")
+workspace = os.getenv("COMET_WORKSPACE")
+
+experiment = comet_ml.Experiment(
+    api_key=api_key,
+    project_name=project_name,
+    workspace=workspace,
+)
 
 # Use 4 classes for parts detection: bird, head, eye, beak
 model = RFDETRNano(num_classes=4)
 
 dataset_dir = "../data/cub_coco_parts"
-model.train(dataset_dir=dataset_dir, epochs=1, batch_size=8, grad_accum_steps=2)
+model.train(dataset_dir=dataset_dir, epochs=100)
