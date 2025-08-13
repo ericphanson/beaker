5d90ec1ddf358983764f3d803cd1cb6916f335b8
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-08-13 23:50:51 +0200

    mv/wip

---
+++ detect_rfdetr/onnx_export/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from onnx-export!")
+
+
+if __name__ == "__main__":
+    main()
--- detect_rfdetr/onnx_export/test_orient.py
+++ detect_rfdetr/onnx_export/test_orient.py
@@ -6,7 +6,7 @@ model.model.reinitialize_detection_head(5)
 
 # output3 trained w detect head
 checkpoint = torch.load(
-    "output3/checkpoint_best_regular.pth", map_location="cpu", weights_only=False
+    "../output3/checkpoint_best_regular.pth", map_location="cpu", weights_only=False
 )
 model.model.model.load_state_dict(checkpoint["model"], strict=True)
 
@@ -15,3 +15,5 @@ x = torch.randn(1, 3, 800, 800)
 y = model.model.model.forward(x)
 
 y["pred_orient"]
+
+help(model.export())
