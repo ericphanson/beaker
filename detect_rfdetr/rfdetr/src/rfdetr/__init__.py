fca30b0bff9c375d86f8050e9cc77cf97e83e3ca
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-08-14 17:09:31 +0200

    use src layout

--- detect_rfdetr/rfdetr/src/rfdetr/__init__.py
+++ detect_rfdetr/rfdetr/src/rfdetr/__init__.py
@@ -11,3 +11,5 @@ if os.environ.get("PYTORCH_ENABLE_MPS_FALLBACK") is None:
     os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 from rfdetr.detr import RFDETRBase, RFDETRLarge, RFDETRNano, RFDETRSmall, RFDETRMedium
+
+__all__ = ["RFDETRBase", "RFDETRLarge", "RFDETRNano", "RFDETRSmall", "RFDETRMedium"]
