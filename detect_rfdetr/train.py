928cdc16bb37b129bc01987f313d0a84cc6f8223
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-08-15 14:08:36 +0200

    try training medium

--- detect_rfdetr/train.py
+++ detect_rfdetr/train.py
@@ -1,7 +1,7 @@
 import comet_ml
 import os
 
-from rfdetr import RFDETRNano
+from rfdetr import RFDETRMedium
 
 api_key = os.getenv("COMET_API_KEY")
 
@@ -14,8 +14,16 @@ experiment = comet_ml.Experiment(
     workspace=workspace,
 )
 
+output_dir = "output5"
+
+if os.path.isdir(output_dir):
+    raise Exception("Output dir already exists")
+
+# resolution = 560
 # Use 4 classes for parts detection: bird, head, eye, beak
-model = RFDETRNano(num_classes=4)
+model = RFDETRMedium(
+    num_classes=4, class_names=["bird", "head", "eye", "beak"], num_queries=100
+)
 
 # actually re-init head! Otherwise has 90 classes:
 assert model.model.model.class_embed.out_features == 91
@@ -24,4 +32,5 @@ model.model.reinitialize_detection_head(5)
 assert model.model.model.class_embed.out_features == 5
 
 dataset_dir = "../data/cub_coco_parts"
-model.train(dataset_dir=dataset_dir, epochs=50, output_dir="output4")
+# assert model.get_model_config().resolution == resolution
+model.train(dataset_dir=dataset_dir, epochs=100, output_dir=output_dir)
